877317600b0ba757c51fc6e475a30a28d23a8d5a
Will Gilman <will@samadhi.tech>
2022-04-19 09:25:55 -0700

    Add server shutdown and boltdb skeleton

--- svr/urlshort/urlshort.go
+++ svr/urlshort/urlshort.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"example.com/handler"
+	// "github.com/boltdb/bolt"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
@@ -35,6 +41,26 @@ func main() {
 	mux.HandleFunc("/hello", hello)
 	mux.HandleFunc("/headers", headers)
 
+	// db, err := bolt.Open("svr.db", 0600, &bolt.Options{Timeout: 1 * time.Second})
+	// if err != nil {
+	// 	fmt.Errorf("could not open db: %s", err)
+	// }
+	// defer db.Close()
+	//
+	// db.Update(func(tx *bolt.Tx) error {
+	// 	_, err := tx.CreateBucket([]byte("PathUrl"))
+	// 	if err != nil {
+	// 		return fmt.Errorf("create bucket: %s", err)
+	// 	}
+	// 	return nil
+	// })
+	//
+	// db.Update(func(tx *bolt.Tx) error {
+	// 	b := tx.Bucket([]byte("PathUrl"))
+	// 	err := b.Put([]byte("/disney"), []byte("https://www.disney.com"))
+	// 	return err
+	// })
+
 	// pathsToUrls := map[string]string{
 	// 	"/google": "https://google.com",
 	// }
@@ -47,24 +73,65 @@ func main() {
 	// `
 
 	json := `[
-      {
-        "path":"/erlang",
-        "url":"https://www.erlang.org/"
-      },
-      {
-        "path":"/hacker",
-        "url":"https://news.ycombinator.com/"
-      }
-    ]
-  `
+	    {
+	      "path":"/erlang",
+	      "url":"https://www.erlang.org/"
+	    },
+	    {
+	      "path":"/hacker",
+	      "url":"https://news.ycombinator.com/"
+	    }
+	  ]
+	`
 
 	// mapHandler := handler.MapHandler(pathsToUrls, mux)
 	// yamlHandler, err := handler.YamlHandler([]byte(yaml), mux)
 	jsonHandler, err := handler.JsonHandler([]byte(json), mux)
+	// dbHandler, err := handler.DbHandler(db, mux)
 	if err != nil {
 		log.Fatal(err)
 		os.Exit(1)
 	}
 
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", *port), jsonHandler))
+	server := &http.Server{
+		Addr:    fmt.Sprintf(":%s", *port),
+		Handler: jsonHandler,
+	}
+
+	go start(server)
+
+	stopCh, closeChanF := createChannel()
+	defer closeChanF()
+	log.Println("notified:", <-stopCh)
+
+	shutown(context.Background(), server)
+}
+
+func start(server *http.Server) {
+	log.Println("server started...")
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		panic(err)
+	} else {
+		log.Println("server stopped gracefully")
+	}
+}
+
+func shutown(ctx context.Context, server *http.Server) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		panic(err)
+	} else {
+		log.Println("server shutdown")
+	}
+}
+
+func createChannel() (chan os.Signal, func()) {
+	stopChan := make(chan os.Signal, 1)
+	signal.Notify(stopChan, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	return stopChan, func() {
+		close(stopChan)
+	}
 }
